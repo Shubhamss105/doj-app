a5c4dd9bba5edcc474bb875fda3df988fe5dd86f
Shubhamss105 <shubhamsingh.ss.1407@gmail.com>
2024-11-14 09:35:33 +0530

    splash screen modified

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -4,26 +4,26 @@ import { Animated, Image, View, StyleSheet, Dimensions } from 'react-native';
 const { height } = Dimensions.get('window');
 
 const SplashScreen = () => {
-  const translateY = useRef(new Animated.Value(-height)).current; // Start above the screen
-  const bgColor = useRef(new Animated.Value(0)).current; // For white-to-#FC261B background color transition
+  const translateY = useRef(new Animated.Value(-height)).current; 
+  const bgColor = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    // Run the animation sequence
+
     Animated.sequence([
-      // Move the circle to the bottom of the screen
+
       Animated.timing(translateY, {
-        toValue: height / 2, // Move to the bottom of the screen
+        toValue: height / 2, 
         duration: 800,
         useNativeDriver: true,
       }),
-      // Move the circle back up to the center
+
       Animated.spring(translateY, {
-        toValue: 0, // Center position
+        toValue: 0, 
         useNativeDriver: true,
         bounciness: 10,
         speed: 8,
       }),
-      // Change background color to #FC261B after the circle reaches the center
+
       Animated.timing(bgColor, {
         toValue: 1,
         duration: 1000,
@@ -32,10 +32,9 @@ const SplashScreen = () => {
     ]).start();
   }, [bgColor, translateY]);
 
-  // Interpolate background color from white to #FC261B
   const backgroundColor = bgColor.interpolate({
     inputRange: [0, 1],
-    outputRange: ['#FFFFFF', '#FC261B'], // from white to #FC261B
+    outputRange: ['#FFFFFF', '#FC261B'], 
   });
 
   return (
